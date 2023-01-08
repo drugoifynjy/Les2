8a4c416802321913a48d9b5916f945cda90d829e
fynjy <drugoifynjy@yandex.ru>
2023-01-08 20:28:54 +0600

    f

---
+++ les2/les/abstract_models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+
+class LocalityType(models.Model):
+    """Тип населенного пункта: locality_type = ['деревня', 'село', 'город', 'посёлок']"""
+    locality_type = models.CharField(max_length=20, blank=True, null=True, verbose_name='Тип населенноо пункта', default='село')
+    locality_type_sokr = models.CharField(max_length=5, blank=True, null=True,
+                                          verbose_name='Тип населенноо пункта кратко', default='с.')
+
+    def __str__(self):
+        a = str(self.locality_type)
+
+        return a
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Тип населенного пункта'
+        verbose_name_plural = 'Типы населенных пунктов'
+
+
+class Address(models.Model):
+    postcode = models.CharField(max_length=6, blank=True, null=True, verbose_name='Индекс')
+    region = models.CharField(max_length=100, blank=True, null=True, default='Омская', verbose_name='Область')
+    district = models.CharField(max_length=50, blank=True, null=True, verbose_name='Район')
+    locality_type = models.ForeignKey(LocalityType, on_delete=models.SET_NULL,
+                                      blank=True, null=True, verbose_name='Тип населенного пункта')
+    locality = models.CharField(max_length=50, blank=True, null=True, verbose_name='Населенный пункт')
+    street = models.CharField(max_length=50, blank=True, null=True, verbose_name='Улица')
+    house_number = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Дом')
+    apartment_number = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Квартира')
+
+    def __str__(self):
+        if self.apartment_number:
+            a = str(self.postcode.__str__()) + ' ' + \
+                str(self.region.__str__()) + ' область ' + \
+                str(self.district.__str__()) + ' район ' + \
+                str(self.locality_type.locality_type_sokr) + \
+                str(self.locality.__str__()) + ' ул. ' + \
+                str(self.street.__str__()) + ' д. ' + \
+                str(self.house_number.__str__()) + ' кв. ' + \
+                str(self.apartment_number.__str__())
+        else:
+            a = str(self.postcode.__str__()) + ' ' + \
+                str(self.region.__str__()) + ' область ' + \
+                str(self.district.__str__()) + ' район ' + \
+                str(self.locality_type) + \
+                str(self.locality.__str__()) + ' ул. ' + \
+                str(self.street.__str__()) + ' д. ' + \
+                str(self.house_number.__str__())
+        return a
+
+    class Meta:
+        abstract = True
+
+
+class Passport(models.Model):
+    series = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name='Серия')
+    number = models.PositiveIntegerField(blank=True, null=True, verbose_name='Номер')
+    date_of_issue = models.DateField(blank=True, null=True, verbose_name='Дата выдачи')
+    issued = models.CharField(max_length=250, blank=True, null=True, verbose_name='Кем выдан')
+    address_birth = models.TextField(max_length=500, blank=True, null=True, verbose_name='Место рождения')
+    inn = models.PositiveIntegerField(blank=True, null=True, verbose_name='ИНН')
+
+    def __str__(self):
+        a = str(self.series.__str__()) + ' ' \
+            + str(self.number.__str__()) + ' '\
+            + str(self.date_of_issue.__str__()) + ' '\
+            + str(self.issued.__str__()) + ' '\
+            + str(self.address_birth.__str__()) + ' '
+        return a
+
+    class Meta:
+        abstract = True
+
+
+class PersonAbstr(models.Model):
+    second_name = models.CharField(max_length=50, blank=True, null=True, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=50, blank=True, null=True, verbose_name='Имя')
+    patronymic = models.CharField(max_length=50, blank=True, null=True, verbose_name='Отчество')
+    date_of_bird = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    phone_number = models.IntegerField(blank=True, null=True, verbose_name='Телефон')
+
+
+    class Meta:
+        abstract = True
--- les2/les/models.py
+++ les2/les/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .abstract_models import *
 
 
 class ValidityPeriod(models.Model):
@@ -6,6 +7,51 @@ class ValidityPeriod(models.Model):
     end_date = models.DateField(null=True)
 
 
+class FormOfTheCabin(models.Model):
+    form_of_the_cabin = models.TextField(null=True)
+
+
+class TypeOfCabin(models.Model):
+    type_of_cabin = models.TextField(null=True)
+
+
+class TreeSpecies(models.Model):
+    tree_species = models.CharField(null=True)
+
+
+class TreeSpecies(models.Model):
+    tree_species = models.CharField(null=True)
+
+
 class Organization(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True)
     validity_period = models.ForeignKey('ValidityPeriod', on_delete=models.CASCADE, null=True)
+
+
+class DetailsOfTheOrganization(models.Model):
+    organization = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
+    inn = models.IntegerField(null=True)
+    kpp = models.IntegerField(null=True)
+    ogrn = models.IntegerField(null=True)
+    okpo = models.IntegerField(null=True)
+    email = models.CharField(max_length=255, null=True)
+    validity_period = models.ForeignKey('ValidityPeriod', on_delete=models.CASCADE, null=True)
+
+
+class BankDetail(models.Model):
+    organization = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
+    bank_title = models.TextField(null=True)
+    payment_account = models.IntegerField(null=True)
+    corset = models.IntegerField(null=True)
+    bik = models.IntegerField(null=True)
+    validity_period = models.ForeignKey('ValidityPeriod', on_delete=models.CASCADE, null=True)
+
+
+class Forestry(models.Model):
+    organization = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=255, null=True)
+    validity_period = models.ForeignKey('ValidityPeriod', on_delete=models.CASCADE, null=True)
+
+
+class DepartmentAddress(models.Model):
+    forestry = models.ForeignKey('Forestry', on_delete=models.CASCADE, null=True)
